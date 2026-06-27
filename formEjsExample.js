8e0a4a5f182af75bdee6b10c5f98a0f6e4044afe
PoojaMishra360 <mishrapooja881@gmail.com>
2026-06-27 21:23:43 +0530

    ejs example2

--- formEjsExample.js
+++ formEjsExample.js
@@ -12,6 +12,14 @@ app.set('view engine', 'ejs');
    const { username, email } = req.body;
    res.render('submit-user', { username, email });
  });
+ app.get('/user-list', (req, res) => {
+   const users = [
+     { username: 'JohnDoe', email: 'john.doe@example.com' },
+     { username: 'JaneSmith', email: 'jane.smith@example.com' },
+     { username: 'BobJohnson', email: 'bob.johnson@example.com' }
+   ];
+   res.render('user-list', { users });
+ });
 
  app.listen(5000, () => {
    console.log('Server is running on port 5000');
