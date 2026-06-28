5dd5cf4eb41ca1b2b368859c1d600c710d9ab2cb
PoojaMishra360 <mishrapooja881@gmail.com>
2026-06-28 19:58:27 +0530

    dyanamic routes with json

---
+++ dyanamicRoute.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import userJson from './userJson.json' with { type: 'json' };
+
+const app = express();
+const PORT = 4000;
+
+app.get('/users', (req, res) => {
+  res.json(userJson);
+});
+
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+    const user = userJson.find(u => u.id === parseInt(userId));
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+});
+
+
+app.get('/users/:name', (req, res) => {
+  const userName = req.params.name;
+  const user = userJson.find(u => u.name === userName);
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+  res.json(user);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
